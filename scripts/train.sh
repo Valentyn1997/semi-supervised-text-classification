093b3c26e400fde4107ae675219b1a2c072b3b1b
Valentyn1997 <valik.melnichuk@gmail.com>
2020-09-29 12:00:30 +0000

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -8,5 +8,5 @@ source venv/bin/activate
 export PYTHONPATH=$PYTHONPATH:./
 #module load cuda42/toolkit
 ssh -N -f -L localhost:5000:localhost:5000 ubuntu@10.195.1.127
-python3 ./runnables/train_ssl.py -m data.path='data/REVIEWS-clean/in-topic' optimizer.learning_rate=1e-5 exp.task_name=SL3 optimizer.auto_lr_find=False exp.logging=True exp.gpus="-1" exp.early_stopping_patience=10 data.load_from_cache=True
+python3 ./runnables/train_ssl.py +setting=ssl data.path='data/REVIEWS-clean/in-topic' optimizer.lr=1e-5 exp.task_name=SL3 optimizer.auto_lr_find=False exp.logging=True exp.gpus="-1" exp.early_stopping_patience=10 data.load_from_cache=True
 
